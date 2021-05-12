8e6da53411e62b92cea005bff745ece3967eecf3
loginsoft-integrations <81212299+loginsoft-integrations@users.noreply.github.com>
2021-05-12 21:38:12 +0530

    value

--- misp_modules/modules/expansion/farsight_passivedns.py
+++ misp_modules/modules/expansion/farsight_passivedns.py
@@ -44,7 +44,7 @@ moduleconfig = ['apikey', 'server', 'limit', 'flex_queries']
 
 DEFAULT_DNSDB_SERVER = 'https://api.dnsdb.info'
 DEFAULT_LIMIT = 10
-DEFAULT_DISTRIBUTION_SETTING = Distribution.your_organisation_only
+DEFAULT_DISTRIBUTION_SETTING = Distribution.your_organisation_only.value
 TYPE_TO_FEATURE = {
     "btc": "Bitcoin address",
     "dkim": "domainkeys identified mail",
