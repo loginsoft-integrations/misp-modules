da1895bd802ba48debb5d65217bc8a04ab6d0383
loginsoft-integrations <81212299+loginsoft-integrations@users.noreply.github.com>
2021-05-12 19:44:33 +0530

    update 12

--- misp_modules/modules/expansion/farsight_passivedns.py
+++ misp_modules/modules/expansion/farsight_passivedns.py
@@ -2,7 +2,7 @@ import dnsdb2
 import json
 from . import check_input_attribute, standard_error_message
 from datetime import datetime
-from pymisp import MISPEvent, MISPObject
+from pymisp import MISPEvent, MISPObject, Distribution
 
 misperrors = {'error': 'Error'}
 standard_query_input = [
@@ -43,7 +43,7 @@ moduleconfig = ['apikey', 'server', 'limit', 'flex_queries']
 
 DEFAULT_DNSDB_SERVER = 'https://api.dnsdb.info'
 DEFAULT_LIMIT = 10
-org_distribution = '0'
+DEFAULT_DISTRIBUTION_SETTING = your_organisation_only
 TYPE_TO_FEATURE = {
     "btc": "Bitcoin address",
     "dkim": "domainkeys identified mail",
